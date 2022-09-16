a6e589237787d6eb033dbbab3cc0dc7fb4478f31
Fidel <92132155+FidelBarreiros@users.noreply.github.com>
2022-09-16 14:00:21 -0300

    final update #1

--- menu.js
+++ menu.js
@@ -1,14 +1,14 @@
 jQuery('document'). ready(function($){
-    /**/
+    
 
 
-
+    /*Creacion de variables*/
     var menupress = $('.menu'),
         menu = $('.nav ul'),
         a = $('.carlitos'),
         b = $('.sex a')
 
-
+    /*Función para el menu desplegable*/
     menupress.click(function(){
 
         if(menu.hasClass('show')){
